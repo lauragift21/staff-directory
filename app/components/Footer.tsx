9756e416e49661e85765ad00fbcbb312c33b5b7c
lauragift21 <laurandidi21@gmail.com>
2024-03-14 08:41:13 +0100

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer class="fixed bottom-0 left-0 z-20 w-full p-4 border-t border-gray-200 shadow md:p-6 dark:bg-gray-800 dark:border-gray-600 md:flex md:items-center md:justify-around">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Staff Hub</a>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Staff Directory</a>. All Rights Reserved.
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
