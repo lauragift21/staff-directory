fa8e40b38cc8e49ecbe0c9f16a2aea68a45570a2
lauragift21 <laurandidi21@gmail.com>
2024-03-18 12:15:10 +0100

    fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 border-t border-gray-200 shadow md:p-6 dark:bg-gray-800 dark:border-gray-600 md:flex md:items-center md:justify-around">
+    <footer className="flex justify-between p-4 border-gray-200 md:p-6 dark:bg-gray-800 dark:border-gray-600">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" className="hover:underline">Staff Directory</a>. All Rights Reserved.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
