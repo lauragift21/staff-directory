f56709249beba77e9e6ffcb952e38eeeb5bcbd59
lauragift21 <laurandidi21@gmail.com>
2024-03-18 16:39:01 +0100

    adjust spacing

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,7 +6,7 @@ export const Nav = () => {
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Staff Hub Logo" />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Staff Directory</span>
         </a>
-        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div class="flex md:order-2 space-x-3 space-y-3 md:space-x-0 md:space-y-0 rtl:space-x-reverse">
           <a href="https://github.com/lauragift21/staff-directory" class="text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-indigo-600">Fork on GitHub</a>
         </div>
       </div>
