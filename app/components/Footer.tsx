5b9c1463a4e0559590092e5760b56f52cd09f51b
lauragift21 <laurandidi21@gmail.com>
2024-03-18 12:20:32 +0100

    fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,13 @@
 export const Footer = () => {
   return (
-    <footer className="flex justify-between p-4 border-gray-200 md:p-6 dark:bg-gray-800 dark:border-gray-600">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" className="hover:underline">Staff Directory</a>. All Rights Reserved.
-      </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+    <footer className="w-full p-4 border-gray-200 md:p-6 dark:bg-gray-800 dark:border-gray-600 md:flex md:items-center md:justify-between">
+      <span className="block text-xs text-gray-500 sm:text-sm md:text-base lg:text-md text-center md:text-left dark:text-gray-400">© 2024 <a href="#" className="hover:underline">Staff Directory</a>. All Rights Reserved.</span>
+      <ul className="flex flex-wrap items-center justify-center mt-3 text-xs sm:text-sm md:text-base lg:text-md font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <p className="me-4 md:me-6">Made with HonoX + Cloudflare Pages + D1 + ❤️</p>
+          <p className="mr-4 md:mr-6">Made with ❤️ by HonoX + Cloudflare Pages + D1</p>
         </li>
         <li>
-          <a href="https://github.com/lauragift21/staff-hub" className="hover:underline me-4 md:me-6">GitHub</a>
+          <a href="https://github.com/lauragift21/staff-hub" className="hover:underline mr-4 md:mr-6">GitHub</a>
         </li>
       </ul>
     </footer>
